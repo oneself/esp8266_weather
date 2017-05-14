c71e1412e029a2561698a2b5c4a8e8a3e6ea4134
Eyal Erez <oneself@gmail.com>
2017-05-14 17:59:19 -0400

    Adding LED_COUNT

--- esp8266_weather_config.h
+++ esp8266_weather_config.h
@@ -7,7 +7,8 @@ const char* PASSWORD = "<PASSWORD>";
 const char* LOCATION = "<LON>,<LAT>";
 const char* KEY      = "<API KEY>";
 
-const int PIN_PIR   = 12;
-const int PIN_PIXEL = 13;
+const int PIN_PIR   = <PIN>;
+const int PIN_PIXEL = <PIN>;
+const int LED_COUNT = <COUNT>;
 
 #endif
